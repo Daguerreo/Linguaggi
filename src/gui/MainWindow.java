30290bab057ebc64675e2b88bbbffec9afb27b6b
Orfeo Ciano <orfeo18@gmail.com>
2015-01-12 00:17:30 +0100

    * Removed GridbagLayout to use BorderLayout

--- src/gui/MainWindow.java
+++ src/gui/MainWindow.java
@@ -16,6 +16,11 @@ import javax.swing.ScrollPaneConstants;
 
 import parser.ParseManager;
 import java.awt.Dimension;
+import java.awt.BorderLayout;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
+import javax.swing.border.EtchedBorder;
 
 public class MainWindow {
 
@@ -63,48 +68,15 @@ public class MainWindow {
 
 		JMenuItem mntmSave = new JMenuItem("Save");
 		mnMenu.add(mntmSave);
-		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[] { 0, 248, -1, 380, 0, 262, 100 };
-		gridBagLayout.rowHeights = new int[] { 14, 530, 100 };
-		gridBagLayout.columnWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0,
-				0.0, Double.MIN_VALUE };
-		gridBagLayout.rowWeights = new double[] { 0.0, 0.0, Double.MIN_VALUE };
-		frmCardCreator.getContentPane().setLayout(gridBagLayout);
-
-		Component verticalStrut = Box.createVerticalStrut(20);
-		GridBagConstraints gbc_verticalStrut = new GridBagConstraints();
-		gbc_verticalStrut.insets = new Insets(0, 0, 5, 5);
-		gbc_verticalStrut.gridx = 2;
-		gbc_verticalStrut.gridy = 0;
-		frmCardCreator.getContentPane().add(verticalStrut, gbc_verticalStrut);
-
-		Component verticalStrut_1 = Box.createVerticalStrut(20);
-		GridBagConstraints gbc_verticalStrut_1 = new GridBagConstraints();
-		gbc_verticalStrut_1.insets = new Insets(0, 0, 5, 5);
-		gbc_verticalStrut_1.gridx = 4;
-		gbc_verticalStrut_1.gridy = 0;
-		frmCardCreator.getContentPane().add(verticalStrut_1,
-				gbc_verticalStrut_1);
-
-		Component verticalStrut_2 = Box.createVerticalStrut(20);
-		GridBagConstraints gbc_verticalStrut_2 = new GridBagConstraints();
-		gbc_verticalStrut_2.insets = new Insets(0, 0, 0, 5);
-		gbc_verticalStrut_2.gridx = 0;
-		gbc_verticalStrut_2.gridy = 1;
-		frmCardCreator.getContentPane().add(verticalStrut_2,
-				gbc_verticalStrut_2);
+		frmCardCreator.getContentPane().setLayout(new BorderLayout(0, 0));
 
 		JScrollPane scrollPaneSx = new JScrollPane();
+		scrollPaneSx.setViewportBorder(new EmptyBorder(10, 10, 10, 10));
 		scrollPaneSx
 				.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
 		scrollPaneSx
 				.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-		GridBagConstraints gbc_scrollPaneSx = new GridBagConstraints();
-		gbc_scrollPaneSx.fill = GridBagConstraints.BOTH;
-		gbc_scrollPaneSx.insets = new Insets(0, 0, 0, 5);
-		gbc_scrollPaneSx.gridx = 1;
-		gbc_scrollPaneSx.gridy = 1;
-		frmCardCreator.getContentPane().add(scrollPaneSx, gbc_scrollPaneSx);
+		frmCardCreator.getContentPane().add(scrollPaneSx, BorderLayout.WEST);
 
 		StatPanel leftpanel = new StatPanel();
 		guimanager.setStatPanel(leftpanel);
@@ -112,22 +84,15 @@ public class MainWindow {
 		scrollPaneSx.setViewportView(leftpanel);
 
 		CardPanel cardpanel = new CardPanel();
+		cardpanel.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
 		guimanager.setCardPanel(cardpanel);
 		cardpanel.setGuiManager(guimanager);
-		GridBagConstraints gbc_cardpanel = new GridBagConstraints();
-		gbc_cardpanel.insets = new Insets(0, 0, 0, 5);
-		gbc_cardpanel.fill = GridBagConstraints.BOTH;
-		gbc_cardpanel.gridx = 3;
-		gbc_cardpanel.gridy = 1;
-		frmCardCreator.getContentPane().add(cardpanel, gbc_cardpanel);
+		frmCardCreator.getContentPane().add(cardpanel);
 
 		JScrollPane scrollPaneDx = new JScrollPane();
+		scrollPaneDx.setViewportBorder(new EmptyBorder(10, 10, 10, 10));
 		scrollPaneDx.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-		GridBagConstraints gbc_scrollPaneDx = new GridBagConstraints();
-		gbc_scrollPaneDx.fill = GridBagConstraints.BOTH;
-		gbc_scrollPaneDx.gridx = 5;
-		gbc_scrollPaneDx.gridy = 1;
-		frmCardCreator.getContentPane().add(scrollPaneDx, gbc_scrollPaneDx);
+		frmCardCreator.getContentPane().add(scrollPaneDx, BorderLayout.EAST);
 
 		GrammarPanel rightpanel = new GrammarPanel();
 		rightpanel.getTextFieldGrammar().setMinimumSize(new Dimension(110, 22));
